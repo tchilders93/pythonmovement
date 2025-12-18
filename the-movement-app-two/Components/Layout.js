2974456f2915701d3f0ca8f644bf5f2c145475a8
tchilders93 <tchilders93@gmail.com>
2025-12-17 22:18:53 -0500

    Delete the-movement-app-two directory

--- the-movement-app-two/Components/Layout.js
+++
@@ -1,8 +0,0 @@
-export default function Layout({ children }) {
-  return (
-    <div style={{ padding: "20px", fontFamily: "Arial" }}>
-      <h1>The Movement App</h1>
-      {children}
-    </div>
-  );
-}
